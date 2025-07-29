15f970a94a3676650c248c76e52deeb2394b5cb4
Felipe Augusto Oliveira Dantas <felipedantas184@gmail.com>
2025-07-29 11:23:11 -0300

    remove isrouterready

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,41 +2,26 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import store from '../redux/store'
-import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { AuthContextProvider } from "@/context/AuthContext";
 import ProtectedRoute from "@/context/ProtectedRoutes";
 
-function useRouterReady() {
-  const [isReady, setIsReady] = useState(false);
-  const router = useRouter();
-
-  useEffect(() => {
-    setIsReady(router.isReady);
-  }, [router.isReady]);
-
-  return isReady;
-}
-
-const AuthRequired = ['/auth/register', '/auth/dashboard']
+const AuthRequired = ['/auth/register', '/auth/dashboard'];
 
 export default function App({ Component, pageProps }: AppProps) {
-  const isRouterReady = useRouterReady();
-  const router = useRouter()
+  const router = useRouter();
 
-  return isRouterReady ? (
+  return (
     <AuthContextProvider>
-      {!AuthRequired.includes(router.pathname) ? (
-        <Provider store={store} >
+      <Provider store={store}>
+        {!AuthRequired.includes(router.pathname) ? (
           <Component {...pageProps} />
-        </Provider>
-      ) : (
-        <Provider store={store} >
+        ) : (
           <ProtectedRoute>
             <Component {...pageProps} />
           </ProtectedRoute>
-        </Provider>
-      )}
+        )}
+      </Provider>
     </AuthContextProvider>
-  ) : null
+  );
 }
