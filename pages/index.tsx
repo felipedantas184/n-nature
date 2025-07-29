316a1515f4cb7e23dd30493a14267fe01b7e5e5a
Felipe Augusto Oliveira Dantas <felipedantas184@gmail.com>
2025-07-29 10:49:10 -0300

    up: indexnew

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,19 +31,24 @@ export default function StorePage({ products }: { products: Product[] }) {
     <>
       <Head>
         <title>Ag Prata 925 | Você Sempre Linda</title>
-        <meta name="description" content="Joias confeccionadas em Prata 925 com garantia vitalicia sobre a autenticidade do metal." />
+        <meta name="description" content="Joias confeccionadas em Prata 925 com garantia vitalícia sobre a autenticidade do metal." />
         <meta name="viewport" content="width=device-width,initial-scale=1.0" />
 
-        <meta property="og:title" content="Ag Prata 925 | Você Sempre Linda"/>
-        <meta property="og:description" content="Joias confeccionadas em Prata 925 com garantia vitalicia sobre a autenticidade do metal." />
+        {/* Open Graph */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://agprata.vercel.app/" />
+        <meta property="og:title" content="Ag Prata 925 | Você Sempre Linda" />
+        <meta property="og:description" content="Joias confeccionadas em Prata 925 com garantia vitalícia sobre a autenticidade do metal." />
         <meta property="og:image" content="https://agprata.vercel.app/assets/icons/apple-touch-icon.png" />
-        <meta property="og:image:width" content="400" />
-        <meta property="og:image:height" content="400" />
-        <meta property="og:site_name" content="Ag Prata 925 | Você Sempre Linda"/>
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:site_name" content="Ag Prata 925" />
 
-        <meta property="twitter:title" content="Ag Prata 925 | Você Sempre Linda"/>
-        <meta property="twitter:description" content="Joias confeccionadas em Prata 925 com garantia vitalicia sobre a autenticidade do metal." />
-        <meta property="twitter:image" content="https://agprata.vercel.app/assets/icons/apple-touch-icon.png" />
+        {/* Twitter Cards */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Ag Prata 925 | Você Sempre Linda" />
+        <meta name="twitter:description" content="Joias confeccionadas em Prata 925 com garantia vitalícia sobre a autenticidade do metal." />
+        <meta name="twitter:image" content="https://agprata.vercel.app/assets/icons/apple-touch-icon.png" />
       </Head>
 
       <Layout>
